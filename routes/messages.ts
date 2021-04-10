b0e50d6da2a4d78be6ef42fe85aab0c8493a1fd3
cafeed28 <artlekseev@gmail.com>
2021-04-10 15:04:44 +0500

    delete gj messages

--- routes/messages.ts
+++ routes/messages.ts
@@ -18,7 +18,7 @@ async function router(router: any, options: any) {
 		const gjp = body.gjp;
 		const accountID = body.accountID;
 		let messageID = body.messageID;
-		let messages = body.messages || '';
+		let messages = body.messages;
 
 		if (GJCrypto.gjpCheck(gjp, accountID)) {
 			if (messages) {
@@ -48,6 +48,58 @@ async function router(router: any, options: any) {
 			return '1';
 		} else {
 			fc.error(`Удаление сообщений ${messages} не выполнено: ошибка авторизации`);
+			return '-1';
+		}
+	});
+
+	router.post(`/${config.basePath}/deleteGJMessages20.php`, async (req: any, res: any) => {
+		const requredKeys = ['messageID', 'accountID', 'gjp'];
+		const body = req.body;
+		if (!WebHelper.checkKeys(body, requredKeys)) {
+			fc.error(`Запрос должен иметь эти ключи: ${requredKeys.join(', ')}`);
+			return res.code(400).send('-1');
+		}
+
+		const gjp = body.gjp;
+		let accountID = body.accountID;
+		let messageID = body.messageID;
+		let isSender = body.isSender;
+
+		if (GJCrypto.gjpCheck(gjp, accountID)) {
+			let message = await Mongoose.messages.findOne({
+				messageID: messageID,
+				$or: [{
+					senderID: accountID,
+					recipientID: accountID
+				}]
+			});
+
+			if (!message) {
+				fc.error(`Удаление сообщения ${messageID} не выполнено: сообщение не найдено`);
+				return '-1';
+			}
+
+			if (isSender) {
+				await Mongoose.messages.updateOne({
+					messageID: messageID,
+					recipientID: accountID
+				}, {
+					isNew: 1
+				});
+
+				accountID = message.senderID;
+				isSender = 0;
+			} else {
+				isSender = 1;
+				accountID = message.recipientID;
+			}
+
+			let user = await Mongoose.users.findOne({ accountID: accountID });
+
+			fc.success(`Скачивание сообщения ${messageID} выполнено`);
+			return `6:${user.userName}:3:${user.accountID}:2:${user.accountID}:1:${message.messageID}:4:${message.subject}:8:${message.isNew}:9:${isSender}:5:${message.body}:7:uploadDate`;
+		} else {
+			fc.error(`Удаление сообщения ${messageID} не выполнено: ошибка авторизации`);
 			return '-1';
 		}
 	});
